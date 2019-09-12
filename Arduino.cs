f185cc80f822870533d86186307e566f3ffe676e
arpangupta1923 <42449856+arpangupta1923@users.noreply.github.com>
2019-09-12 22:18:46 +0530

    Add files via upload

---
+++ Arduino.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Arduino : MonoBehaviour
+{
+    
+
+    public Text text;
+
+    
+    
+
+    void   Start()
+    {
+         StartCoroutine(Wait());
+       
+        
+    }
+    void Print_Text(int value)
+    {
+        text.text = value.ToString(); 
+       Rect GraphRect;
+    float xSize = 0.5f * Screen.width;
+    float ySize = 0.1f * Screen.height;
+    float xPos = 0.2f * Screen.width;
+    float yPos = 0.05f * Screen.height;
+
+    GraphRect = new Rect(xPos, yPos, xSize, ySize);
+    GraphManager.Graph.Plot("Test_ScreenSpace", value, Color.green, GraphRect);
+   }
+
+    IEnumerator Wait()
+    {
+        int value = Random.Range(60, 90);
+        Debug.Log("Hi1");
+        yield return new WaitForSeconds(0.5f);
+        Print_Text(value);
+        StartCoroutine(Wait());
+        Debug.Log("hi2");
+
+        
+    }
+
+}
+
---
+++ InputController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class InputController
+{
+    public float CurrentValue;
+
+   
+    float _minInputY = 0f;
+    float _maxInputY = 30;
+
+    string ipaddress = "192.168.43.238";
+    float _minFinalY = -4f;
+    float _maxFinalY = 4;
+
+    public void Begin(string ipAddress, int port)
+    {
+        var thread = new Thread(() =>
+        {
+            
+
+          
+            var client = new TcpClient();
+
+            
+            client.Connect(ipAddress, port);
+            var stream = new StreamReader(client.GetStream());
+
+            
+            var buffer = new List<byte>();
+            while (client.Connected)
+            {
+               
+                var read = stream.Read();
+
+                
+                if (read == 13)
+                {
+                    
+                    var str = Encoding.ASCII.GetString(buffer.ToArray());
+
+                    
+                    var dist = float.Parse(str);
+
+                    dist = Mathf.Clamp(dist, _minInputY, _maxInputY);
+
+                    
+
+                    
+
+                    buffer.Clear();
+                }
+                else
+                    buffer.Add((byte)read);
+            }
+        });
+
+        thread.Start();
+    }
+}
